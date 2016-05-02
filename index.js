755b9b3aa40f63609489b600778c67055b1e030d
Vladimir Starkov <iamstarkov@gmail.com>
2016-05-02 11:27:38 +0200

    implement thing

--- index.js
+++ index.js
@@ -3,5 +3,12 @@
  * @return {Boolean}
  */
 module.exports = function (filename) {
+  if (typeof filename !== 'string') {
+    throw new TypeError([
+      '`filename` should be an `String`,',
+      'but got `' + (typeof filename) + '`: ' + filename,
+    ].join(' '));
+  }
+
   return filename[0] === '.';
 };
